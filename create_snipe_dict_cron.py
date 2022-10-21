8e6daf4069f5293acca4dd31cf14105c2cde2f4c
najirod <100681846+najirod@users.noreply.github.com>
2022-10-21 18:20:47 +0200

    added logging

--- create_snipe_dict_cron.py
+++ create_snipe_dict_cron.py
@@ -22,7 +22,7 @@ logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s:%(pathname)s:%(funcName)s:%(name)s:%(process)d:%(message)s')
 
-file_handler = logging.FileHandler(((sys.path[0])+"/")+'logs/cron_py.log')
+file_handler = logging.FileHandler(((sys.path[1])+"/")+'logs/cron_py.log')
 file_handler.setFormatter(formatter)
 
 stream_handler = logging.StreamHandler()
@@ -35,7 +35,7 @@ logger.addHandler(stream_handler)
 
 class Snipe:
     def __init__(self):
-        root_path = (sys.path[0] + "/")  # /Users/dpustahija1/PycharmProjects/os-snipe_diff/
+        root_path = (sys.path[1] + "/")  # /Users/dpustahija1/PycharmProjects/os-snipe_diff/
         dotenv_path = (root_path + ".env")
         # print(dotenv_path)
         load_dotenv(dotenv_path=dotenv_path)
