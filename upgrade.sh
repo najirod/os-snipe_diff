d2060a95a69436c3df116ee8abcc6d207f6859d4
najirod <dorijan.pustahija@sofascore.com>
2023-02-24 14:18:23 +0100

    added upgrade

---
+++ upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /var/www/os-snipe_diff/venv/bin/activate
+
+# Pull from Git
+cd /var/www/os-snipe_diff/
+git pull
+
+# Install requirements
+pip install -r requirements.txt
+
+# Set permissions
+sudo chmod -R 777 /var/www/os-snipe_diff/
+
+# Restart Apache2 server
+sudo service apache2 restart
